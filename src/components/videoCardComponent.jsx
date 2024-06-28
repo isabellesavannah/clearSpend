e40a723a7df228fd1036be1a7eeb878db55d77fa
IsabelleThomas <isabellethomas@truckstop.com>
2024-06-28 12:46:26 -0600

    video and card in correct position

--- src/components/videoCardComponent.jsx
+++ src/components/videoCardComponent.jsx
@@ -5,23 +5,29 @@ import { Button } from 'react-bootstrap';
 const VideoCardComponent = () => {
   return (
     <div classname="container">
-      <div class="row">
-        <div class="col-12 " >
-          <Button class="d-flex justify-content-end" style={{ backgroundColor: 'white', color: '#04324E' }}>Contact Us</Button>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-12">
-          <video className="video-card" controls>
-            <source src="<path-to-your-video>/DemoVideo.mp4" type="video/mp4" />
-          </video>
-        </div>
-        <div class="col-12">
-          <div className="government-transparency-card">
-            <div>Gov Trans</div>
+      <header>
+        <div class="row" className='contact-us-button'>
+          <div class="col-6" >
+            <Button class="d-flex justify-content-end" style={{ backgroundColor: 'white', color: '#04324E' }}>Contact Us</Button>
           </div>
         </div>
-      </div>
+      </header>
+      <main>
+        <div class="row">
+          <div class="col-12">
+            <div class="card">
+              <iframe
+              className='video-card'
+              title='hero video'
+              src="https://www.youtube.com/embed/Fjo8-ihGJiY?autoplay=1&mute=1&loop=1&controls=0&showinfo=0&autohide=1&disablekb=1">
+              </iframe>
+            </div>
+            <div className="government-transparency-card">
+              <div className="header">Government Transparency That is Easy to Implement and Easy to Use</div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 };
